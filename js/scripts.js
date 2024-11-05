5552a32615b90c626e55b2257c26fcd3f3d756ad
LarsLOL <L.Pluijm05@gmail.com>
2024-11-05 14:41:19 +0100

    Optimizing part one

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,8 @@
-/*!
-* Start Bootstrap - Grayscale v7.0.6 (https://startbootstrap.com/theme/grayscale)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+// Script
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
+function ShowNavbar() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
